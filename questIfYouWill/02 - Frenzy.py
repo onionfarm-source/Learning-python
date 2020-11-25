8b276d13f29dd22e9209506eff0fbb1d4ecb6a0f
ShittyWizard108 <45062402+ShittyWizard108@users.noreply.github.com>
2020-11-26 00:18:52 +0530

    Updated : 2.0

---
+++ questIfYouWill/02 - Frenzy.py
@@ -0,0 +1,22 @@
+# Write a program to find maximum between three numbers.
+
+# Write a program to check whether a number is negative, positive or zero.
+
+# Write a program to check whether a number is even or odd.
+
+# Write a program to check whether a year is leap year or not.
+
+# Write a program to input angles of a triangle and check whether triangle is valid or not
+
+# Write a program to find all roots of a quadratic equation.
+
+# Write a program to calculate profit or loss. And determine which one it is.
+
+########################################################################################################################
+
+# Write the codes below
+# Additional points for modulating each of the above code by a function call for ease of use in future case
+# i.e not having to reinvent the wheel each and every time when needed.
+
+
+
