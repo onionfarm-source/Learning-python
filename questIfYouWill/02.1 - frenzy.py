48c1206410bcc5aec123ce8d0ae4e51788ea0cbd
ShittyWizard108 <45062402+ShittyWizard108@users.noreply.github.com>
2020-11-26 22:41:06 +0530

    Updated : 2.1

---
+++ questIfYouWill/02.1 - frenzy.py
@@ -0,0 +1,26 @@
+# Some question might be nerve racking at times
+# Don't bother using google.Try to solve it by yourself to expand your mind's horizon
+# What better way is there to put your understanding into question aside from tackling questions. XD
+# Don't be dishearten if you fail to solve it, because at first glance, even i couldn't solve some of the questions.
+
+# Write a program to print multiplication table of the number 13 till 12.
+
+# Write a program to find first and last digit of a given number (where the given input should be greater than 10).
+# Note : Numbers in this case are int and looks like 123987123, 981239812309, 128731248 etc
+
+# Write a program to find first and last digit of a given number.
+# Note : Numbers in this case are int and looks like 123987123, 981239812309, 128731248 etc
+
+# Write a program to find sum of digits of a number.
+# Note : Numbers in this case are int and looks like 123987123, 981239812309, 128731248 etc
+
+# Write a program to find reverse of a number.
+
+# Write a program to check if a given number is palindrome or not.
+# Palindrome = Palindrome number is such number which when reversed is equal to the original number.
+# Example : 121, 12321, 1001 etc
+
+# Write a program to find all factor's of a number.
+
+
+# Write the code below:
