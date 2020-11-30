0ca9fbd69140d3aeb1a330c2b822caaa0bb88599
ShittyWizard108 <45062402+ShittyWizard108@users.noreply.github.com>
2020-11-30 08:20:31 +0530

    Update 03 - talkAboutGroups.py

--- questIfYouWill/03 - talkAboutGroups.py
+++ questIfYouWill/03 - talkAboutGroups.py
@@ -2,7 +2,7 @@
 
 # Write a program to find diameter, circumference and area of circle using functions.
 
-# A good day to learn about something new. Armstrong Number ! ! !
+# A good day to learn something new. Armstrong Number ! ! !
 # Write a program to find whether a number is a Armstrong number using functions.
 
 
