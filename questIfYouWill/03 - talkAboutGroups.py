fcaef9ba9e202dfa04b37963131c328ba9956690
ShittyWizard108 <45062402+ShittyWizard108@users.noreply.github.com>
2020-11-30 08:19:19 +0530

    Updated : 3

--- questIfYouWill/03 - talkAboutGroups.py
+++ questIfYouWill/03 - talkAboutGroups.py
@@ -1,1 +1,10 @@
-# TBD
+# Write a program to find cube of any number using function.
+
+# Write a program to find diameter, circumference and area of circle using functions.
+
+# A good day to learn about something new. Armstrong Number ! ! !
+# Write a program to find whether a number is a Armstrong number using functions.
+
+
+
+
